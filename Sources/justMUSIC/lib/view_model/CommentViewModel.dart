e40560aa7dcf8907f9376aae6e5e369d3c7acce2
emkartal1 <emre.kartal@etu.uca.fr>
2023-07-26 13:45:09 +0200

    add tokenSpotify class :white_check_mark:

---
+++ Sources/justMUSIC/lib/view_model/CommentViewModel.dart
@@ -0,0 +1,10 @@
+import '../model/Comment.dart';
+
+class CommentViewModel {
+
+  // Methods
+  List<Comment> getCommentsPost(int idPost) {
+    throw new Error();
+  }
+
+}
---
+++ Sources/justMUSIC/lib/view_model/MusicViewModel.dart
@@ -0,0 +1,19 @@
+import '../model/Music.dart';
+
+class MusicViewModel {
+
+  final String API_URL = "";
+
+  // Methods
+  Music getMusic(int id) {
+    throw new Error();
+  }
+
+  Music getMusicWithName(String name) {
+    throw new Error();
+  }
+
+  List<Music> getMusics(String name) {
+    throw new Error();
+  }
+}
