ae9ad68030cbb886388da4917990e179b64bab47
emkartal1 <emre.kartal@etu.uca.fr>
2023-07-27 12:51:03 +0200

    Add new functions in the MusicViewModel class :hammer:

--- Sources/justMUSIC/lib/model/Music.dart
+++ Sources/justMUSIC/lib/model/Music.dart
@@ -6,11 +6,12 @@ class Music {
   String? _cover;
   String? _previewUrl;
   DateTime? _date;
+  double? _duration;
   List<Artist> _artists;
 
   // Constructor
   Music(this._id, this._title, this._cover, this._previewUrl, this._date,
-      this._artists);
+      this._duration, this._artists);
 
   //Getters and setters
   String? get id => _id;
@@ -37,6 +38,12 @@ class Music {
 
   set date(DateTime? value) {
     _date = value;
+  }
+
+  double? get duration => _duration;
+
+  set duration(double? value) {
+    _duration = value;
   }
 
   List<Artist> get artists => _artists;
--- Sources/justMUSIC/test/Music_test.dart
+++ Sources/justMUSIC/test/Music_test.dart
@@ -7,7 +7,7 @@ Future<void> main() async {
   Music m = await musicVM.getMusic('295SxdR1DqunCNwd0U767w');
 
   print("id : ${m.id.toString()}, cover : ${m.cover}, title : ${m.title}");
-  print("date : ${m.date.toString()}, preview : ${m.previewUrl}");
+  print("date : ${m.date.toString()}, preview : ${m.previewUrl}, duration : ${m.duration}");
   for (Artist a in m.artists) {
     print("id : ${a.id}, name : ${a.name}");
   }
@@ -17,7 +17,7 @@ Future<void> main() async {
   List<Music> musics = await musicVM.getMusicsWithName('Shavkat');
   for (Music m in musics) {
     print("id : ${m.id.toString()}, cover : ${m.cover}, title : ${m.title}");
-    print("date : ${m.date.toString()}, preview : ${m.previewUrl}");
+    print("date : ${m.date.toString()}, preview : ${m.previewUrl}, duration : ${m.duration}");
     for (Artist a in m.artists) {
       print("id : ${a.id}, name : ${a.name}");
     }
@@ -28,9 +28,33 @@ Future<void> main() async {
   List<Music> musics2 = await musicVM.getMusicsWithArtistName('jul');
   for (Music m in musics2) {
     print("id : ${m.id.toString()}, cover : ${m.cover}, title : ${m.title}");
-    print("date : ${m.date.toString()}, preview : ${m.previewUrl}");
+    print("date : ${m.date.toString()}, preview : ${m.previewUrl}, duration : ${m.duration}");
     for (Artist a in m.artists) {
       print("id : ${a.id}, name : ${a.name}");
     }
   }
+
+  print('\nArtist with Name:');
+
+  Artist a = await musicVM.getArtistWithName('jul');
+  print("id : ${a.id}, name : ${a.name}, image : ${a.image}");
+
+  print('\nArtists with Name:');
+
+  List<Artist> artists = await musicVM.getArtistsWithName('jul');
+  for (Artist a in artists) {
+    print("id : ${a.id}, name : ${a.name}, image : ${a.image}");
+  }
+
+  print('\nTop Musics :');
+
+  List<Music> topMusics = await musicVM.getTopMusicsWithArtistId('3NH8t45zOTqzlZgBvZRjvB');
+  for (Music m in topMusics) {
+    print("id : ${m.id.toString()}, cover : ${m.cover}, title : ${m.title}");
+    print("date : ${m.date.toString()}, preview : ${m.previewUrl}, duration : ${m.duration}");
+    for (Artist a in m.artists) {
+      print("id : ${a.id}, name : ${a.name}");
+    }
+  }
+
 }
